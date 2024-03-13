bd5a6de8e638cb4a586f248c403ca2a9b97c7001
ege aydın <egeaydinn31@gmail.com>
2024-03-13 19:57:21 +0300

    first commit

---
+++ Labs/NetCore8MVC/Controllers/ArabaController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore8MVC.Models;
+
+
+
+namespace NetCore8MVC.Controllers
+{
+    public class ArabaController : Controller
+    {
+      
+        public ArabaController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+          
+            TCar araba = new TCar(); 
+            araba.Marka = "Ford"; 
+            araba.Model = "Kuga";
+            araba.ModelYil = 2020;
+            araba.Renk = "Beyaz"
+
+            return View(TCar); 
+        }
+        
+        public IActionResult Ekle()
+        {
+            return View();
+        }z<
+    }
+}
---
+++ Labs/NetCore8MVC/Models/car.cs
@@ -0,0 +1,11 @@
+﻿namespace NetCore8MVC.Models
+{
+    public class TCar
+    {
+        //Property kısayol => prop + tab + tab
+        public string Marka { get; set; }
+        public int Model { get; set; }
+        public decimal ModelYil { get; set; }
+        public decimal Renk { get; set; }
+    }
+}
